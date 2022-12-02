cff2689758e4f777505729828a5581b318b8c889
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-02 17:57:47 +0530

    Stage 1 completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/App.css';
 const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
-const Join = (props) =>{
-  return(
-    <div id ="join">
+const Join = (props) => {
+  return (
+    <div id="join">
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
+      {props.words.join(',')}
     </div>
   )
 }
@@ -12,7 +13,7 @@ const App = () => {
 
   return (
     <div id="main">
-      <Join  />
+      <Join words={arr} />
     </div>
   )
 }
